f97271c7768a624ba345a5dfa9dfd578c3bf1fc7
Andrea Bigagli <bigagli.andrea@hotmail.it>
2023-06-16 21:38:49 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace ExifViewer
+{
+    partial class ExifViewer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExifViewer));
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRotate90Back = new System.Windows.Forms.Button();
+            this.buttonRotation90Right = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonShowImage = new System.Windows.Forms.Button();
+            this.buttonExif = new System.Windows.Forms.Button();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonAdd.Location = new System.Drawing.Point(7, 13);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(118, 46);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add Image";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRotate90Back
+            // 
+            this.buttonRotate90Back.Image = ((System.Drawing.Image)(resources.GetObject("buttonRotate90Back.Image")));
+            this.buttonRotate90Back.Location = new System.Drawing.Point(144, 13);
+            this.buttonRotate90Back.Name = "buttonRotate90Back";
+            this.buttonRotate90Back.Size = new System.Drawing.Size(47, 46);
+            this.buttonRotate90Back.TabIndex = 7;
+            this.buttonRotate90Back.UseVisualStyleBackColor = true;
+            this.buttonRotate90Back.Click += new System.EventHandler(this.buttonRotate90Back_Click);
+            // 
+            // buttonRotation90Right
+            // 
+            this.buttonRotation90Right.Image = ((System.Drawing.Image)(resources.GetObject("buttonRotation90Right.Image")));
+            this.buttonRotation90Right.Location = new System.Drawing.Point(197, 13);
+            this.buttonRotation90Right.Name = "buttonRotation90Right";
+            this.buttonRotation90Right.Size = new System.Drawing.Size(47, 46);
+            this.buttonRotation90Right.TabIndex = 8;
+            this.buttonRotation90Right.UseVisualStyleBackColor = true;
+            this.buttonRotation90Right.Click += new System.EventHandler(this.buttonRotation90Right_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Location = new System.Drawing.Point(12, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 0);
+            this.panel1.TabIndex = 9;
+            // 
+            // buttonShowImage
+            // 
+            this.buttonShowImage.Location = new System.Drawing.Point(250, 22);
+            this.buttonShowImage.Name = "buttonShowImage";
+            this.buttonShowImage.Size = new System.Drawing.Size(142, 37);
+            this.buttonShowImage.TabIndex = 11;
+            this.buttonShowImage.Text = "Show Image";
+            this.buttonShowImage.UseVisualStyleBackColor = true;
+            this.buttonShowImage.Click += new System.EventHandler(this.buttonShowImage_Click);
+            // 
+            // buttonExif
+            // 
+            this.buttonExif.Location = new System.Drawing.Point(398, 22);
+            this.buttonExif.Name = "buttonExif";
+            this.buttonExif.Size = new System.Drawing.Size(142, 37);
+            this.buttonExif.TabIndex = 12;
+            this.buttonExif.Text = "EXIF DATA";
+            this.buttonExif.UseVisualStyleBackColor = true;
+            this.buttonExif.Click += new System.EventHandler(this.buttonExif_Click);
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxImage.Location = new System.Drawing.Point(12, 86);
+            this.pictureBoxImage.MaximumSize = new System.Drawing.Size(512, 512);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(512, 453);
+            this.pictureBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxImage.TabIndex = 2;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(520, 338);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // ExifViewer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(544, 564);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonExif);
+            this.Controls.Add(this.buttonShowImage);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonRotation90Right);
+            this.Controls.Add(this.buttonRotate90Back);
+            this.Controls.Add(this.buttonAdd);
+            this.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Name = "ExifViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ExifViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRotate90Back;
+        private System.Windows.Forms.Button buttonRotation90Right;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonShowImage;
+        private System.Windows.Forms.Button buttonExif;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GdPicture14;
+using GdPicture;
+
+
+namespace ExifViewer
+{
+    public partial class ExifViewer : Form
+    {
+        Image image;
+        int height;
+        int width;
+        int countBack90 = 0;
+        int countForward90 = 0;
+        public ExifViewer()
+        {
+            InitializeComponent();
+            GdPicture14.LicenseManager oLicenseManager = new GdPicture14.LicenseManager();
+            oLicenseManager.RegisterKEY("0471518396633296756441224");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBoxImage.Visible = false;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            countBack90 = 0;
+            countForward90 = 0;
+            
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                //carico immagine principale
+                image = new Bitmap(open.FileName);
+                height = (int)(image.Height * 0.16);
+                width = (int)(image.Width * 0.16);
+                pictureBoxImage.Size = new System.Drawing.Size(width, height);
+                pictureBoxImage.SizeMode = PictureBoxSizeMode.CenterImage;
+                pictureBoxImage.BorderStyle = BorderStyle.Fixed3D;
+                pictureBoxImage.Image = image;                
+                pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBoxImage.Visible = true;
+
+                // Estraggo dati EXIF.
+                GdPictureImaging oGdPictureImaging = new GdPictureImaging();
+                int ImageID = oGdPictureImaging.CreateGdPictureImageFromFile(open.FileName);
+                if (oGdPictureImaging.GetStat() == GdPictureStatus.OK)
+                {
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    sb.AppendLine("FILE PATH: "+open.FileName);
+                    int exifTagsCount = oGdPictureImaging.TagCount(ImageID);
+                    if (exifTagsCount != 0)
+                    {
+                        //EXIF tags.
+                            sb.AppendLine("EXIF DATA:\n-----------\n");
+                            for (int i = 1; i <= exifTagsCount - 1; i++)
+                            {
+                                string tagName = oGdPictureImaging.TagGetName(ImageID, i);
+                                string tagFormattedValue = oGdPictureImaging.TagGetValueString(ImageID, i);
+                                sb.AppendLine(tagName + ":      " + tagFormattedValue + "\n");
+                            }
+                                oGdPictureImaging.ReleaseGdPictureImage(ImageID);
+                                textBox1.Text = sb.ToString();                   
+                    
+                    }
+                    else
+                    {
+                        sb.AppendLine("NO EXIF DATA");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The image can't be loaded. Status: " + oGdPictureImaging.GetStat().ToString(), "Metadata Example", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+           
+        }
+
+        private void buttonRotate90Back_Click(object sender, EventArgs e)
+        {
+
+
+            countBack90++;
+            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+
+            //per la rotazione inverto altezza e larghezza.
+            if (countBack90%2==1 && countForward90%2==0)
+                pictureBoxImage.Size = new System.Drawing.Size(height,width);
+            else if(countBack90 % 2 == 1 && countForward90 % 2 == 1)
+                pictureBoxImage.Size = new System.Drawing.Size(width,height);
+            else if (countBack90 % 2 == 0 && countForward90 % 2 == 0)
+                pictureBoxImage.Size = new System.Drawing.Size(width, height);
+            else if (countBack90 % 2 == 0 && countForward90 % 2 == 1)
+                pictureBoxImage.Size = new System.Drawing.Size(height, width);
+
+
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBoxImage.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxImage.Image = image;
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImage.Image = image;
+        }
+
+        private void buttonRotation90Right_Click(object sender, EventArgs e)
+        {
+          
+            countForward90++; 
+
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            if (countBack90 % 2 == 0 && countForward90 % 2 == 1)
+                pictureBoxImage.Size = new System.Drawing.Size(height, width);
+            else if (countBack90 % 2 == 0 && countForward90 % 2 == 0)
+                pictureBoxImage.Size = new System.Drawing.Size(width, height);
+            else if (countBack90 % 2 == 1 && countForward90 % 2 == 1)
+                pictureBoxImage.Size = new System.Drawing.Size(width, height);
+            else if (countBack90 % 2 == 1 && countForward90 % 2 == 0)
+                pictureBoxImage.Size = new System.Drawing.Size(height, width);
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBoxImage.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxImage.Image = image;
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImage.Image = image;
+
+        }
+
+
+        private void buttonShowImage_Click(object sender, EventArgs e)
+        {
+            pictureBoxImage.Visible = true;
+            textBox1.Visible = false;
+        }
+
+        private void buttonExif_Click(object sender, EventArgs e)
+        {
+            pictureBoxImage.Visible = false;
+            textBox1.Visible = true;
+        }
+    }
+}
